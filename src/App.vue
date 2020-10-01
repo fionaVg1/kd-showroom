ef4590f18d893e2a43122aaaba809e07ecd4b1fb
fiona <1125575769@qq.com>
2020-10-01 10:23:20 +0800

    根据屏幕大小自适应

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <template>
-  <div id="app">    
-    <router-view/>
+  <div id="app">
+    <router-view />
   </div>
 </template>
+<script>
+/**
+ * 根据屏幕大小自适应
+ * px/rem = 20
+ */
+function resize() {
+  var setPro = 1;
+  (function(doc, win) {
+    var docEl = doc.documentElement,
+      resizeEvt =
+        "orientationchange" in window ? "orientationchange" : "resize",
+      recalc = function() {
+        var clientWidth = docEl.clientWidth;
+        if (!clientWidth) return;
+        setPro = clientWidth / 960;
+        docEl.style.fontSize = 10 * setPro + "px";
+      };
+    recalc();
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener("DOMContentLoaded", recalc, false);
+  })(document, window);
+}
+resize();
+</script>
 
 <style lang="scss">
 #app {
